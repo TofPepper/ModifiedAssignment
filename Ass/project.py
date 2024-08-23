b49a073c1a80f71ebec73d15aec3371298bb22a9
TofPepper <etofunmi3@gmail.com>
2024-08-23 15:59:34 +0100

    First commit

---
+++ Ass/project.py
@@ -0,0 +1,19 @@
+Name = "My name is Emmanuel Tofunmi Funmilayo"
+Age = 19
+gender = "Female"
+school = "Federal College of Education Technical Akoka Yaba Lagos, in affiliation with the University of Benin city (UNIBEN)"
+print(Name)
+print(f"I am {Age} years old")
+print(f"I am a {gender}")
+print(f"I school at {school}")
+
+print("I recently got a scholarship at PowerLearnProject, and it has been engaging since I joined")
+
+for i in range(3):
+    print(f"really {i}")
+
+
+print("Though it made learning more fun and encouraging")
+
+
+ 
